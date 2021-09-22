537270355a8df36b353f18e3ab2abd63518825cf
nobody <nobody@example.com>
2021-09-22 23:57:09 +0900

    add makefile for mingw

--- Makefile.mingw.mk
+++ Makefile.mingw.mk
@@ -1,23 +1,56 @@
 CC=x86_64-w64-mingw32-gcc
 AR=x86_64-w64-mingw32-ar
-CFLAGS=-Wall -O3 `/usr/local/x86_64-w64-mingw32/bin/sdl2-config --cflags`
-INCLUDE_PATHS=-I /usr/local/x86_64-w64-mingw32/include
+CFLAGS=-Wall -O3 -Dmain=SDL_main
+INCLUDE_PATHS=-I build/x86_64-w64-mingw32/SDL/include/SDL2
 
-BUILD_DIR=build/mingw
-SOURCES = $(wildcard src/*.c)
-OBJECTS = $(addprefix $(BUILD_DIR)/, $(notdir $(SOURCES:.c=.o)))
-TARGET=$(BUILD_DIR)/libbismite-core.a
+TARGET=build/x86_64-w64-mingw32/libbismite.a
+OBJ_DIR=build/x86_64-w64-mingw32/objs
+SOURCES = $(wildcard src/*.c) $(wildcard src/ext/*.c)
+OBJECTS = $(SOURCES:src/%.c=$(OBJ_DIR)/%.o)
 
-all: $(BUILD_DIR) $(TARGET)
+SDL_TGZ=build/x86_64-w64-mingw32/SDL-x86_64-w64-mingw32.tgz
+SDL_TGZ_URL=https://github.com/bismite/SDL-x86_64-w64-mingw32/releases/download/0.2.0/SDL-x86_64-w64-mingw32.tgz
+SDL_DIR=build/x86_64-w64-mingw32/SDL
 
-$(BUILD_DIR):
+SAMPLE_DIR=build/x86_64-w64-mingw32/samples
+SAMPLE_SOURCES = $(wildcard samples/src/*.c)
+SAMPLE_EXES = $(SAMPLE_SOURCES:samples/src/%.c=$(SAMPLE_DIR)/%.exe)
+SAMPLE_LDFLAGS=-Lbuild/x86_64-w64-mingw32/SDL/lib -Lbuild/x86_64-w64-mingw32/SDL/bin -Lbuild/x86_64-w64-mingw32
+SAMPLE_LIBS = -lbismite -lmingw32 -lSDL2main -lSDL2 -mwindows -lSDL2_mixer -lSDL2_image -lopengl32
+SAMPLE_ASSETS = $(wildcard samples/assets/**/*)
+
+# ----
+
+all: $(OBJ_DIR) $(SDL_DIR) $(TARGET)
+samples: all $(SAMPLE_DIR) $(SAMPLE_EXES) copyassets copysdl
+clean:
+	rm -rf build/x86_64-w64-mingw32
+
+$(SDL_TGZ):
+	$(shell ./scripts/download.sh $(SDL_TGZ_URL) $(SDL_TGZ))
+
+$(SDL_DIR): $(SDL_TGZ)
 	mkdir -p $@
+	tar --strip-component 1 -zxf $(SDL_TGZ) -C $@
 
-$(BUILD_DIR)/%.o: src/%.c
+$(OBJ_DIR):
+	mkdir -p $@/ext
+
+$(OBJ_DIR)/%.o: src/%.c
 	$(CC) -c $^ -o $@ -I include $(CFLAGS) $(INCLUDE_PATHS)
 
 $(TARGET): $(OBJECTS)
 	$(AR) rcs $@ $^
 
-clean:
-	rm -rf $(BUILD_DIR)
+# ----
+
+$(SAMPLE_DIR):
+	mkdir -p $@
+
+$(SAMPLE_DIR)/%.exe: samples/src/%.c
+	$(CC) $^ -o $@ -I include $(CFLAGS) $(INCLUDE_PATHS) $(SAMPLE_LIBS) $(SAMPLE_LDFLAGS)
+
+copyassets:
+	cp -R samples/assets $(SAMPLE_DIR)
+copysdl:
+	cp -R build/x86_64-w64-mingw32/SDL/bin/*.dll build/x86_64-w64-mingw32/SDL/licenses $(SAMPLE_DIR)

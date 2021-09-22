be0a61b99d957e545f94288fcef3f0842da21f32
nobody <nobody@example.com>
2021-09-22 15:40:37 +0900

    add makefile for linux

--- Makefile.linux.mk
+++ Makefile.linux.mk
@@ -3,21 +3,41 @@ AR=ar
 CFLAGS=-Wall -O3 `sdl2-config --cflags`
 INCLUDE_PATHS=
 
-BUILD_DIR=build/linux
-SOURCES = $(wildcard src/*.c)
-OBJECTS = $(addprefix $(BUILD_DIR)/, $(notdir $(SOURCES:.c=.o)))
-TARGET=$(BUILD_DIR)/libbismite-core.a
+TARGET=build/linux/libbismite.a
+OBJ_DIR=build/linux/objs
+SOURCES = $(wildcard src/*.c) $(wildcard src/ext/*.c)
+OBJECTS = $(SOURCES:src/%.c=$(OBJ_DIR)/%.o)
 
-all: $(BUILD_DIR) $(TARGET)
+SAMPLE_DIR=build/linux/samples
+SAMPLE_SOURCES = $(wildcard samples/src/*.c)
+SAMPLE_EXES = $(SAMPLE_SOURCES:samples/src/%.c=$(SAMPLE_DIR)/%.exe)
+SAMPLE_CFLAGS=
+SAMPLE_LIBS = `sdl2-config --libs` -lSDL2_image -lSDL2_mixer -lbismite -lm -lGL
+SAMPLE_ASSETS = $(wildcard samples/assets/**/*)
 
-$(BUILD_DIR):
-	mkdir -p $@
+# ----
 
-$(BUILD_DIR)/%.o: src/%.c
+all: $(OBJ_DIR) $(TARGET)
+samples: all $(SAMPLE_DIR) $(SAMPLE_EXES) copyassets
+clean:
+	rm -rf build/linux
+
+$(OBJ_DIR):
+	mkdir -p $@/ext
+
+$(OBJ_DIR)/%.o: src/%.c
 	$(CC) -c $^ -o $@ -I include $(CFLAGS) $(INCLUDE_PATHS)
 
 $(TARGET): $(OBJECTS)
 	$(AR) rcs $@ $^
 
-clean:
-	rm -rf $(BUILD_DIR)
+# ----
+
+$(SAMPLE_DIR):
+	mkdir -p $@
+
+$(SAMPLE_DIR)/%.exe: samples/src/%.c
+	$(CC) $^ -o $@ -I include $(CFLAGS) $(SAMPLE_CFLAGS) $(INCLUDE_PATHS) $(SAMPLE_LIBS) -Lbuild/linux
+
+copyassets:
+	cp -R samples/assets $(SAMPLE_DIR)

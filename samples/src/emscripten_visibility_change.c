b5b18858272b1730ef5c70aafc5ef4f2d60fceb6
kbys <work4kbys@gmail.com>
2021-12-15 07:17:04 +0900

    add sample: emscripten_visibility_change.c

---
+++ samples/src/emscripten_visibility_change.c
@@ -0,0 +1,34 @@
+#include "common.h"
+#include <bi/ext/action.h>
+
+BiLayer *layer=NULL;
+
+#ifdef EMSCRIPTEN
+#include <emscripten/html5.h>
+EM_BOOL visibilitychange_callback(int eventType,
+                                  const EmscriptenVisibilityChangeEvent *visibilityChangeEvent,
+                                  void *userData)
+{
+  printf("visibilitychange_callback\n");
+  if(layer) bi_set_color(layer->root->color, 0xff,0,0xff,64);
+  return EM_TRUE;
+}
+#endif
+
+int main(int argc, char* argv[])
+{
+  BiContext* context = bi_init_context(ALLOC(BiContext),480,320,0,false,__FILE__);
+  print_info(context);
+
+#ifdef EMSCRIPTEN
+  emscripten_set_visibilitychange_callback(context,true,visibilitychange_callback);
+#endif
+
+  layer = bi_layer_init(ALLOC(BiLayer));
+  layer->root = make_sprite_with_anchor("assets/check.png",0,0);
+  layer->textures[0] = layer->root->texture_mapping->texture;
+  bi_add_layer(context,layer);
+
+  bi_start_run_loop(context);
+  return 0;
+}

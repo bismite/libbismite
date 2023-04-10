708cdfdef2266f7702b69dbdcd071e2f53321389
kbys <work4kbys@gmail.com>
2023-04-10 20:26:29 +0900

    fix ScaleTo action

--- samples/src/action_scale.c
+++ samples/src/action_scale.c
@@ -11,25 +11,44 @@ int main(int argc, char* argv[])
   BiContext* context = bi_init_context(ALLOC(BiContext),480,320,0,false,__FILE__);
   print_info(context);
 
+  // sprite
+  BiTexture *texture = MAKE_TEXTURE("assets/face01.png");
   // layer
   BiLayer *layer = bi_layer_init(ALLOC(BiLayer));
   layer->root = make_sprite_with_anchor("assets/check.png",0,0);
   bi_add_layer(context,layer);
-
-  // sprite
-  BiNode* face = make_sprite("assets/face01.png");
-  bi_node_set_position(face,240,160);
-  bi_node_add_node(layer->root,face);
   layer->textures[0] = layer->root->texture;
-  layer->textures[1] = face->texture;
+  layer->textures[1] = texture;
 
-  // x0.5
-  bi_node_set_scale(face,0.5,0.5);
-  // Scale
-  BiActionScale* scale = bi_action_scale_by_init(ALLOC(BiActionScale),500,4,4);
-  bi_add_action(face,(BiAction*)scale);
-  bi_action_start((BiAction*)scale);
-  scale->action.on_finish = callback;
+  // face green
+  BiNode* face_green = make_sprite_from_texture(texture);
+  bi_set_color(face_green->color, 0,0xff,0,0xff);
+  bi_node_set_position(face_green,240,160);
+  bi_node_add_node(layer->root,face_green);
+  // face red
+  BiNode* face_red = make_sprite_from_texture(texture);
+  bi_set_color(face_red->color, 0xff,0,0,0xff);
+  bi_node_set_position(face_red,120,80);
+  bi_node_add_node(layer->root,face_red);
+  bi_node_set_scale(face_red,0.5,0.5); // x0.5
+  // face blue
+  BiNode* face_blue = make_sprite_from_texture(texture);
+  bi_set_color(face_blue->color, 0,0,0xff,0xff);
+  bi_node_set_position(face_blue,360,240);
+  bi_node_add_node(layer->root,face_blue);
+  bi_node_set_scale(face_blue,0.5,0.5); // x0.5
+
+  // Scale By
+  BiActionScale* scale_by = bi_action_scale_by_init(ALLOC(BiActionScale),5000,2,2);
+  scale_by->action.on_finish = callback;
+  bi_add_action(face_red,(BiAction*)scale_by);
+  bi_action_start((BiAction*)scale_by);
+
+  // Scale To
+  BiActionScale* scale_to = bi_action_scale_to_init(ALLOC(BiActionScale),5000,2,2);
+  scale_to->action.on_finish = callback;
+  bi_add_action(face_blue,(BiAction*)scale_to);
+  bi_action_start((BiAction*)scale_to);
 
   bi_start_run_loop(context);
   return 0;
--- src/ext/action_scale.c
+++ src/ext/action_scale.c
@@ -37,8 +37,8 @@ static void bi_action_scale_to_update(BiAction* action, double rate)
 {
   BiActionScale* scale = (BiActionScale*)action;
   bi_node_set_scale(action->node,
-                    scale->from_x + (scale->dx - scale->from_x) * rate,
-                    scale->from_x + (scale->dx - scale->from_x) * rate);
+                    scale->from_x + (scale->dx-scale->from_x) * rate,
+                    scale->from_x + (scale->dx-scale->from_x) * rate);
 }
 
 static void bi_action_scale_to_start(BiAction* action)

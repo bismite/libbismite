5edcd209047ed86b7fbd3f1cf5ba702244d92f34
kbys <work4kbys@gmail.com>
2024-12-18 20:23:51 +0900

    makefile.mingw.mk

--- Makefile.mingw.mk
+++ Makefile.mingw.mk
@@ -22,13 +22,21 @@ SAMPLE_LDFLAGS=-L$(BUILD_DIR)/bin
 SAMPLE_LIBS = $(BUILD_DIR)/lib/libbismite.a -lmingw32 $(BUILD_DIR)/lib/libSDL2main.a -lSDL2 -mwindows -lSDL2_mixer -lSDL2_image -lopengl32
 SAMPLE_ASSETS = $(wildcard samples/assets/**/*)
 
+TEST_DIR=$(BUILD_DIR)/test
+TEST_SOURCES = $(wildcard test/*.c)
+TEST_EXES = $(TEST_SOURCES:test/%.c=$(TEST_DIR)/%.exe)
+TEST_LDFLAGS = $(SAMPLE_LDFLAGS)
+TEST_LIBS = $(BUILD_DIR)/lib/libbismite.a -lmingw32 $(BUILD_DIR)/lib/libSDL2main.a -lSDL2 -mwindows -lSDL2_mixer -lSDL2_image -lopengl32
+TEST_ASSETS = $(SAMPLE_ASSETS)
+
 ARCHIVE=$(BUILD_DIR)/libbismite-mingw.tgz
 ARCHIVE_SAMPLES=$(BUILD_DIR)/libbismite-mingw-samples.tgz
 
 # ----
 
-all: $(ARCHIVE) $(ARCHIVE_SAMPLES)
-samples: $(TARGET) $(SAMPLE_DIR) $(SAMPLE_EXES) copy_assets copy_libs
+all: $(ARCHIVE) $(ARCHIVE_SAMPLES) test
+samples: copy_assets copy_libs $(SAMPLE_EXES)
+test: copy_test_assets copy_test_libs $(TEST_EXES)
 
 $(BUILD_DIR):
 	mkdir -p $@
@@ -43,16 +51,29 @@ $(OBJ_DIR)/%.o: src/%.c $(LIBSDL2) $(OBJ_DIR)/ext
 $(TARGET): $(OBJECTS)
 	$(AR) rcs $@ $^
 
-# ----
+# ---- Sample
 
-$(SAMPLE_DIR):
-	mkdir -p $@
-$(SAMPLE_DIR)/%.exe: samples/%.c $(TARGET) $(SAMPLE_DIR)
+$(SAMPLE_DIR)/%.exe: samples/%.c $(TARGET)
+	mkdir -p $(SAMPLE_DIR)
 	$(CC) $< -o $@ $(CFLAGS) $(INCLUDE_PATHS) $(SAMPLE_LIBS) $(SAMPLE_LDFLAGS)
 copy_assets:
+	mkdir -p $(SAMPLE_DIR)
 	cp -R samples/assets $(SAMPLE_DIR)
 copy_libs: $(TARGET)
+	mkdir -p $(SAMPLE_DIR)
 	cp -R $(BUILD_DIR)/bin/*.dll $(SAMPLE_DIR)
+
+# ---- Test
+
+$(TEST_DIR)/%.exe: test/%.c $(TARGET)
+	mkdir -p $(TEST_DIR)
+	$(CC) $< -o $@ $(CFLAGS) $(TEST_CFLAGS) $(INCLUDE_PATHS) $(TEST_LIBS) $(TEST_LDFLAGS)
+copy_test_assets:
+	mkdir -p $(TEST_DIR)
+	cp -R samples/assets $(TEST_DIR)
+copy_test_libs: $(TARGET)
+	mkdir -p $(TEST_DIR)/lib
+	cp -R $(BUILD_DIR)/bin/*.dll $(TEST_DIR)
 
 # ----
 
